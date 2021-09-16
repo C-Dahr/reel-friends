9292c4938bafb75d714b35f510e8ebaf5f470004
C-Dahr <cameronsdahr@gmail.com>
2021-09-16 18:46:10 -0300

    Added vuetify theme and decoupled navbar into its own component

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				background: '#FFDAB9', // Not automatically applied
+			},
+			dark: {
+				background: 'indigo', // If not using lighten/darken, use base to return hex
+			},
+		},
+	},
 });
